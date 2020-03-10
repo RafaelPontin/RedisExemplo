f713bd1b6d305af9998e7d57a5c06ad9ab29470b
rafael pontin <rfmpontin@gmail.com>
2020-03-10 19:22:22 -0300

    First Commit

---
+++ ExemploRedis/Form1.cs
@@ -0,0 +1,43 @@
+﻿using ExemploRedis.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploRedis
+{
+    public partial class Form1 : Form
+    {
+        private RedisUtil redisUtil;
+        private string host = "127.0.0.1:6379"; 
+
+        public Form1()
+        {
+            redisUtil = new RedisUtil(host);
+            InitializeComponent();
+        }
+
+        private void btSet_Click(object sender, EventArgs e)
+        {
+            if(!txtChave.Text.Equals("") && !txtValor.Text.Equals(""))
+            {
+                redisUtil.Set(txtChave.Text, txtValor.Text);
+                txtValor.Text = "Sucesso";
+            }
+        }
+
+        private void btGet_Click(object sender, EventArgs e)
+        {
+            if(!txtChave.Text.Equals(""))
+            {
+                string valor = redisUtil.Get(txtChave.Text);
+                txtValor.Text = valor;
+            }
+        }
+    }
+}
---
+++ ExemploRedis/Util/RedisUtil.cs
@@ -0,0 +1,37 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploRedis.Util
+{
+    public class RedisUtil
+    {
+        private readonly string _host;
+
+        public RedisUtil(string host)
+        {
+            _host = host;
+        }
+
+        public void Set(string key, string value)
+        {
+            using (RedisClient redisClient = new RedisClient(_host))
+            {
+                redisClient.Set(key, value);
+            }
+        }
+
+        public string Get(string key)
+        {
+            using (RedisClient redisClient = new RedisClient(_host))
+            {
+                return redisClient.Get<string>(key);
+            }
+        }
+
+
+    }
+}
